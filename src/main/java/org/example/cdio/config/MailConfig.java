96d9449cfb4a680572790d0114df3467d09ff1e6
cuong <you@example.com>
2026-03-06 16:34:54 +0700

    bug

--- src/main/java/org/example/cdio/config/MailConfig.java
+++ src/main/java/org/example/cdio/config/MailConfig.java
@@ -1,12 +1,9 @@
 package org.example.cdio.config;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
-
 
 import java.util.Properties;
 
@@ -24,6 +21,7 @@ public class MailConfig {
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
+
         return mailSender;
     }
 }
