1e5f90f06189f05421754cd54e3fa9fac0915336
linhjunior205 <linhmedia2005@gmail.com>
2026-03-06 18:02:54 +0700

    verssion neww

--- src/main/java/org/example/cdio/controller/OrderController.java
+++ src/main/java/org/example/cdio/controller/OrderController.java
@@ -7,18 +7,22 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/store/order")
 public class OrderController {
 
     private final OrderRepository orderRepository;
 
+    // trang thông báo đơn đã được duyệt
     @GetMapping("/order-approved")
     public String orderApproved(){
         return "order-approved";
     }
 
+    // API kiểm tra trạng thái đơn hàng
     @GetMapping("/api/order-status/{id}")
     @ResponseBody
     public String getStatus(@PathVariable Long id){
+
         return orderRepository.findById(id)
                 .map(o -> o.getStatus().name())
                 .orElse("NOT_FOUND");
